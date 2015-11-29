a9b8909a24617cd26db17018a4d0cf0bd303a226
Bryan Mulvihill <bmulvihill@dresources.com>
2015-11-29 14:46:47 -0500

    Working on final transition probablity matrix

--- supervised_random_walks/transition_matrix.py
+++ supervised_random_walks/transition_matrix.py
@@ -1,7 +1,12 @@
 # Takes edge strength matrix and returns transition matrix
 import numpy as np
 
-def transition_matrix(A):
+def stochastic_transition_matrix(A):
     row_sum = A.sum(axis=1)
     q_prime = np.divide(A, row_sum)
     return q_prime
+
+def final_transition_matrix(Q_prime, alpha=0.2):
+    Q = np.multiply(Q_prime,(1-alpha))
+    # += 1*alpha restart probabilityx
+    return Q
--- tests/transition_matrix_test.py
+++ tests/transition_matrix_test.py
@@ -3,19 +3,25 @@ from supervised_random_walks.transition_matrix import *
 
 class TestTransitonMatrix(object):
     def setup(self):
-        self.tm = transition_matrix(np.matrix([[1,2,3,4],[1,2,3,4]]))
+        self.stm = stochastic_transition_matrix(np.matrix([[1,2,3,4],[1,2,3,4]]))
 
     def teardown(self):
         "Teardown"
 
-    def test_transition_matrix_size(self):
-        assert_equal(self.tm.shape, (2,4))
+    def test_stochastic_transition_matrix_size(self):
+        assert_equal(self.stm.shape, (2,4))
 
-    def test_transition_matrix(self):
-        v = self.tm == [[0.1,0.2,0.3,0.4], [0.1,0.2,0.3,0.4]]
-        assert_equal(self.tm.all(), True)
+    def test_stochastic_transition_matrix(self):
+        v = self.stm == [[0.1,0.2,0.3,0.4], [0.1,0.2,0.3,0.4]]
+        assert_equal(self.stm.all(), True)
 
-    def test_transition_matrix_right_stochastic_matrix(self):
-        row_sum = self.tm.sum(axis=1)
+    def test_stochastic_transition_matrix_returns_stochastic_matrix(self):
+        row_sum = self.stm.sum(axis=1)
         v = row_sum == [[1],[1]]
         assert_equal(v.all(), True)
+
+    def test_final_transition_matrix(self):
+        Q = final_transition_matrix(self.stm)
+        row_sum = Q.sum(axis=1)
+        v = row_sum == [[0.8],[0.8]]
+        assert_equal(v.all(),True)
