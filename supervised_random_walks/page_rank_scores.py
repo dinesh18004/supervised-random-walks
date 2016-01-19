f7d3a20bf45c01811d282a5fce8fdf7c27a44099
Bryan Mulvihill <bmulvihill@dresources.com>
2016-01-18 21:20:53 -0500

    stationary page rank, convergence

--- supervised_random_walks/page_rank_scores.py
+++ supervised_random_walks/page_rank_scores.py
@@ -9,3 +9,24 @@ def initialize_page_rank(adjacency_matrix_cardinality):
 def initialize_partial_derivatives(adjacency_matrix_cardinality, w_cardinality):
     dp = np.zeros((adjacency_matrix_cardinality, w_cardinality))
     return dp
+
+# Takes adjacency_matrix , Final Transition matrix Q
+# Creates stationary page rank vector p
+def page_rank_vector(adjacency_matrix, Q):
+    t = 1
+    n = len(adjacency_matrix)
+    p = initialize_page_rank(n)
+    c = False
+    while not c:
+        p_new = p
+        t += 1
+        for i in range(n):
+            p_new[i] = np.sum(p * Q[:,i])
+        c = all(converged(p, p_new))
+        p = p_new
+    return p
+
+# Covergence method
+# Takes two p stationary matrix and determines if they are the same based on epsilon
+def converged(p1, p2, epsilon = 10**-12):
+    return np.abs(p1 - p2) <= epsilon
--- tests/page_rank_scores_test.py
+++ tests/page_rank_scores_test.py
@@ -1,6 +1,19 @@
 from nose.tools import *
 import numpy as np
 from supervised_random_walks.page_rank_scores import *
+from supervised_random_walks.transition_matrix import *
+
+def adjacency_matrix():
+    am = [[0, 1, 1],
+          [1, 0, 0],
+          [1, 0, 0]]
+    return am
+
+def transition_matrix():
+    stm = stochastic_transition_matrix(np.matrix([[0, 1, 2],
+                                                  [1, 0, 2],
+                                                  [2, 2, 0]]))
+    return stm
 
 def initialize_page_rank_test_returns_a_two_dimensional_array():
     v = initialize_page_rank(2) == np.array([0.5, 0.5])
@@ -9,3 +22,15 @@ def initialize_page_rank_test_returns_a_two_dimensional_array():
 def initialize_partial_derivatives_test_returns_zeroes_matrix():
     v = initialize_partial_derivatives(2, 2) == np.array([[0, 0],[0, 0]])
     assert_equal(v.all(),True)
+
+def test_page_rank_vector():
+    Q = final_transition_matrix(transition_matrix(), alpha=0.3)
+    p = page_rank_vector(adjacency_matrix(), Q)
+    raise Exception(np.multiply(Q, p))
+    #raise Exception(page_rank_vector(adjacency_matrix(), Q))
+
+def test_coverged_returns_true_if_value_is_less_then_epsilon():
+    assert_equal(converged(np.matrix([1]),np.matrix([1])), True)
+
+def test_converged_returns_false_if_value_greater_then_epsilon():
+    assert_equal(converged(np.matrix([1]), np.matrix([2])), False)
